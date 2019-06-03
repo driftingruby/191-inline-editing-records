f044567c640787cba37c5dbd479cf1dd8b330d97
Dave Kimura <dkimura@isolvedhcm.com>
2019-06-03 01:14:43 -0400

    191

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,58 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.js {}
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+      else
+        format.js {}
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email)
+  end
+end
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,4 @@
 const { environment } = require('@rails/webpacker')
-
+// https://github.com/rails/webpacker/issues/2114
+environment.loaders.delete('nodeModules')
 module.exports = environment
